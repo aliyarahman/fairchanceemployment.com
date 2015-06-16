da3fbf652fb849fab8ec616a193eb6bc9add6990
aliyarahman <aliya.rahman@yahoo.com>
2015-06-16 13:25:12 -0400

    Updated model to reflect new categories of RC friendliness

--- fce/app/models.py
+++ fce/app/models.py
@@ -14,9 +14,10 @@ class Industry(models.Model):
         return self.name
 
 
-
 class Employer(models.Model):
     name = models.CharField(max_length=100)
+    short_name = models.CharField(max_length=45)
+    industries = models.ManyToManyField(Industry)
     phone = models.CharField(max_length=40, null=True, blank=True)
     email = models.EmailField(max_length=255, null=True, blank=True)
     hq_address = models.CharField(max_length=255, null=True, blank=True)
@@ -24,51 +25,59 @@ class Employer(models.Model):
     home_url = models.CharField(max_length=255, null=True, blank=True)
     employment_url = models.CharField(max_length=255, null=True, blank=True)
     notes = models.TextField(null=True, blank=True)
-    box_present = models.IntegerField(null=True, blank=True)
-    crime_type_asked = models.IntegerField(null=True, blank = True) # 1 = "crime", 2="misdemeanor", 3="felony"
-    years_specified_by_box = models.IntegerField(null=True, blank = True)
-    industries = models.ManyToManyField(Industry)
-    # vetted = models.BooleanField(default = False)
-    # day_care = models.BooleanField(default = False)
-    # federal_bonding = models.BooleanField(default = False)
-    # retention_programs  = models.BooleanField(default = False)
-    # professional_development = models.BooleanField(default = False)
-    # clean_record_on_abusive_practices = models.BooleanField(default = False)
+    score = models.IntegerField(default = 0)
+    stars = models.IntegerField(default = 0)
 
-    # def calculate_score(self):
+    # Category 1 Criteria
+    c1c1_complies_dc = models.IntegerField(default = 0) #Complies w DC's ban the box law
 
+    # Category 2 Criteria
+    c2c1_friendly_attendance_policy = models.IntegerField(default = 0)
+    c2c2_manager_training = models.IntegerField(default = 0)
 
+    # Category 3 Criteria
+    c3c1_eligible_work_opportunity = models.IntegerField(default = 0)
+    c3c2_participates_work_opportunity = models.IntegerField(default = 0)
+    c3c3_federal_bonding = models.IntegerField(default = 0)
+
+    # Category 4 Criteria
+    c4c1_merit_based_awards = models.IntegerField(default = 0)
+    c4c2_leadership_training = models.IntegerField(default = 0)
+
+    # Category 5 Criteria
+    c5c1_recruitment = models.IntegerField(default = 0)
+    c5c2_supplier_diversity = models.IntegerField(default = 0)
+    c5c3_friendly_marketing = models.IntegerField(default = 0)
+    c5c4_public_advocacy = models.IntegerField(default = 0)
+
+    # Category 6 Criteria
+    c5c1_nature_of_crime = models.IntegerField(default = 0)
+    c5c2_no_housing_history_check = models.IntegerField(default = 0)
+    c5c3_no_residence_loc_review = models.IntegerField(default = 0)
+    c5c4_no_housing_based_disq = models.IntegerField(default = 0)
+    c5c5_no_arrests_inquiry = models.IntegerField(default = 0)
+    c5c6_disregards_conviction_beyond_cutoff = models.IntegerField(default = 0)
+    c5c6num_year_cutoff = models.IntegerField(null=True, blank = True)
+    c5c7_discusses_unfavorable_check = models.IntegerField(default = 0)
+    c5c8_no_automatic_dismiss_on_bg_check = models.IntegerField(default = 0)
+    c5c9_differentiates_violent_offenses = models.IntegerField(default = 0)
+    c5c10_record_is_not_major_barrier = models.IntegerField(default = 0)
+    c5c11_dishonesty_insurance = models.IntegerField(default = 0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-        return self.name
+        return self.short_name
 
 
-class Position(models.Model):
-    employer = models.ForeignKey(Employer)
-    title = models.CharField(max_length=100)
-    phone = models.CharField(max_length=40, null=True, blank=True)
-    worksite_location = models.CharField(max_length=255, null=True, blank=True)
-    job_posted = models.CharField(max_length = 100, null=True, blank = True) #link to the position
-    collateral_sanction_applies = models.BooleanField(default=True)
-    which_collateral_sanction_applies = models.CharField(max_length=200, null=True, blank=True)
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __unicode__(self):
-        return self.title
-
-
-class UserReview(models.Model):
+class UserComment(models.Model):
     author = models.ForeignKey(User)
     employer = models.ForeignKey(Employer)
-    position = models.ForeignKey(Position, null=True, blank=True)
-    title = models.CharField(max_length=40)
-    notes = models.TextField(null=True, blank=True)
-    date_of_action = models.CharField(max_length=40, null=True, blank=True)
+    text = models.TextField(null=True, blank=True)
+    date_posted = models.CharField(max_length=40, null=True, blank=True)
+    upvotes = models.IntegerField(default= 0)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -87,4 +96,4 @@ class BlogPost(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     def __unicode__(self):
-        return self.id
+        return self.title
